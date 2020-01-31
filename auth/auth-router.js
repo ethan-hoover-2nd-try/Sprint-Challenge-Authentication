cac69f4d1de764e745a66eafe475b0257b481d01
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2020-01-31 10:05:27 -0800

    added validation into login and register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const secrets = require("./secrets.js");
 const db = require("./auth-model.js");
 
-router.post("/register", (req, res) => {
+router.post("/register", validate, (req, res) => {
   // implement registration
   const newUser = req.body;
   const hashedPw = bcrypt.hashSync(newUser.password, 12);
@@ -15,13 +15,12 @@ router.post("/register", (req, res) => {
     .catch(error => res.status(500).json({ error: "something went wrong while attempting to register new user" }));
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", validate, (req, res) => {
   // implement login
   let { username, password } = req.body;
 
   db.login(username)
     .then(user => {
-      console.log(user);
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
 
@@ -49,6 +48,19 @@ function generateToken(user) {
   };
 
   return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+function validate(req, res, next) {
+  const data = req.body;
+  if (!data) {
+      res.status(400).json({ error: 'missing username and password' })
+  } else if (!data.username) {
+      res.status(400).json({ error: 'missing required username' })
+  } else if (!data.password) {
+      res.status(400).json({ error: 'missing required password' })
+  } else {
+      next();
+  }
 }
 
 module.exports = router;
